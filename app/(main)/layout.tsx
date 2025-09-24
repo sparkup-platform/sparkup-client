da08467f1e514b8be4977697683336aebdc4dbe5
TeukHee <rlaxmrgml@naver.com>
2025-09-24 17:32:15 +0900

    design: 메인 페이지 레이아웃 디자인 조정

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -4,11 +4,13 @@ import SideBar from "@/components/layout/navigation";
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-row bg-info">
+    <div className="flex flex-row bg-info h-screen">
         <SideBar/>
-        <div className="">
-        <Header />
-        {children}
+        <div className="flex-1 flex flex-col overflow-auto">
+                <Header />
+            <div className="flex flex-col p-[var(--spacing-5)]">
+            {children}
+            </div>
         </div>
     </div>
   )
